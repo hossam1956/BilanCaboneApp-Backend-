4092d45a9ae885bb477a8f35454ed8b577abb1da
chalabi hossam <chalabihossam@chalabis-MacBook-Pro.local>
2024-07-08 15:48:10 +0100

    Configuration initial du backend

---
+++ BilanCarbone/src/main/java/com/example/BilanCarbone/BilanCarboneApplication.java
@@ -0,0 +1,13 @@
+package com.example.BilanCarbone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilanCarboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilanCarboneApplication.class, args);
+	}
+
+}
---
+++ BilanCarbone/src/test/java/com/example/BilanCarbone/BilanCarboneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BilanCarbone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilanCarboneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
