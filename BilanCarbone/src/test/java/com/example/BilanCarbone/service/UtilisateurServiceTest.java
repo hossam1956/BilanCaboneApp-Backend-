fbb4c8929fdd4a6b77734171c54cb5998d659dde
chalabi hossam <chalabihossam@chalabis-MacBook-Pro.local>
2024-09-02 12:22:13 +0100

    Modify config

---
+++ BilanCarbone/src/test/java/com/example/BilanCarbone/service/UtilisateurServiceTest.java
@@ -0,0 +1,52 @@
+package com.example.BilanCarbone.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilisateurServiceTest {
+
+    @Test
+    void fetchEntrepriseOfUtilisateur() {
+    }
+
+    @Test
+    void getAllUtilisateur() {
+    }
+
+    @Test
+    void getAllUtilisateurList() {
+    }
+
+    @Test
+    void getUtilisateurById() {
+    }
+
+    @Test
+    void getUserId() {
+    }
+
+    @Test
+    void getRoleId() {
+    }
+
+    @Test
+    void assignRoleToUser() {
+    }
+
+    @Test
+    void createUtilisateur() {
+    }
+
+    @Test
+    void updateUtilisateur() {
+    }
+
+    @Test
+    void blockUtilisateur() {
+    }
+
+    @Test
+    void deleteUtilisateur() {
+    }
+}
